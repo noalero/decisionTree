3e1a4653c320c24895e56c68a432f48e5f0c5252
noalero <noaleron98@gmail.com>
2024-04-09 17:21:16 +0300

    Added setter and getter for n_breeds

--- feature.py
+++ feature.py
@@ -5,6 +5,7 @@ import string
 class Feature(object):
     def __init__(self, name, values, n_breeds) -> None:
         self.__set_name__(name)
+        self.__set_n_breeds__(n_breeds)
         # self.__set_breeds__(values,n_breeds)
 
     def __set_name__(self, name) -> None:
@@ -12,6 +13,12 @@ class Feature(object):
 
     def get_name(self) -> string:
         return self.name
+
+    def __set_n_breeds__(self, n_breeds) -> None:
+        self.n_breeds = n_breeds
+
+    def get_n_breeds(self) -> int:
+        return self.n_breeds
 
     def __set_breeds__(self, values, n_breeds) -> None:  # visitor pattern
         pass
