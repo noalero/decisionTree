0588789f185fa2cef14a611a08cff47d2f4b421d
noalero <noaleron98@gmail.com>
2024-04-09 16:55:36 +0300

    Implemented __set_breeds__ using set data structure and range

--- numericalFeature.py
+++ numericalFeature.py
@@ -1,21 +1,30 @@
 import feature
-import range
+import brange
 
 
 class NumericalFeature(feature.Feature):
-    def __init__(self, name, index, values) -> None:
+    def __init__(self, name, index, values, n_breeds) -> None:
         feature.Feature.__init__(self, name, index, values)
-        self.__set_breeds__(values)
+        self.__set_breeds__(values, n_breeds)
 
-    def __set_breeds__(self, values) -> None:  # visitor pattern
-        pass
+    def __set_breeds__(self, values, n_breeds) -> None:  # visitor pattern
+        self.breeds = set()
+        min_val = min(values)
+        max_val = max(values)
+        brange_size = (max_val - min_val) / abs(n_breeds)  # if n_breeds < 0: default value
+        edges = (True, False)
+        for i in range(abs(n_breeds) - 1):
+            self.breeds.add(brange.Range(min_val, max_val, edges))
+            min_val = max_val
+            max_val = max_val + brange_size
+        self.breeds.add(brange.Range(min_val, max_val, (True, True)))
 
-    def is_value_of_breed(self, range_, value) -> bool:  # visitor pattern
+    def is_value_of_breed(self, brange_, value) -> bool:  # visitor pattern
         ans = True
-        if value < range_.get_from_index() | value > range_.get_to_index():
+        if value < brange_.get_from_index() | value > brange_.get_to_index():
             ans = False
-        if not (range_.get_edges()[0]) & value == range_.get_from_index():
+        if not (brange_.get_edges()[0]) & value == brange_.get_from_index():
             ans = False
-        if not (range_.get_edges()[1]) & value == range_.get_to_index():
+        if not (brange_.get_edges()[1]) & value == brange_.get_to_index():
             ans = False
         return ans
