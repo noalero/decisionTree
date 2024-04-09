2a0d652e5728f4f4d24495fc7a28b8fbd6d1f3b5
noalero <noaleron98@gmail.com>
2024-04-09 15:46:18 +0300

    Implemented __set_breeds__ using set data structure

--- categoricalFeature.py
+++ categoricalFeature.py
@@ -8,8 +8,10 @@ class CategoricalFeature(feature.Feature):
         self.__set_breeds__(values)
 
     def __set_breeds__(self, values) -> None:  # visitor pattern
-        breeds = []
-        pass
+        self.breeds = set()
+        for val in values:
+            if not (val in self.breeds):
+                self.breeds.add(val)
 
     def is_value_of_breed(self, breed_, value) -> bool:  # visitor pattern
         return breed_ == value
