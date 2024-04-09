f9345b0f2e479d1b73552a1e2ff2a99a7935c2eb
noalero <noaleron98@gmail.com>
2024-04-09 15:03:44 +0300

    Changed signatures

--- dataPath.py
+++ dataPath.py
@@ -4,21 +4,28 @@ import categoricalFeature
 import breed
 import leaf
 import parent
+
+
 class DataPath(object):
-    def __init__(self):
+    def __init__(self) -> None:
         pass
-    def __setSize__(self, size):
+
+    def __set_size__(self, size) -> None:
         self.size = size
-    def getSize(self):
+
+    def get_size(self) -> int:
         return self.size
-    def __setPath__(self, path):
+
+    def __set_path__(self, path) -> None:
         self.path = path
         pass
-    def getPath(self):
+
+    def get_path(self) -> list:
         return self.path
-    def addFeature(self, feature):
+
+    def add_feature(self, feature_) -> None:
         pass
         # resize
-    def addBreed(self, breed):
-        pass
-        # resize
+
+    def add_breed(self, breed_) -> None:
+        pass  # resize
