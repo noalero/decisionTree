9d8ebfb7a03417fa7ad4f60e34b34c9b2f66eed4
noalero <noaleron98@gmail.com>
2024-04-09 14:55:55 +0300

    Changed signatures

--- parent.py
+++ parent.py
@@ -1,11 +1,18 @@
 import breed
+import node
+
+
 class Parent(breed.Breed):
-    def __init__(self, name, dataPath, feature):
-        breed.Breed.__init__(name, dataPath)
-        self.__createNext__(feature, dataPath)
-    def __setNext__(self, next):
-        self.next = next
-    def getNext(self):
+    def __init__(self, name, datapath_, feature_):
+        breed.Breed.__init__(self, name, datapath_)
+        self.__create_next__(feature_, datapath_)
+        pass
+
+    def __set_next__(self, next_) -> None:
+        self.next = next_
+
+    def get_next(self) -> node.Node:
         return self.next
-    def __createNext__(self, feature, dataPath):
+
+    def __create_next__(self, feature, datapath_) -> None:
         pass
