7e8b5dc4f3b66f3849da410cc1c8d57808e85be7
noalero <noaleron98@gmail.com>
2024-04-09 14:51:50 +0300

    Changed signatures

--- breed.py
+++ breed.py
@@ -1,12 +1,19 @@
+import dataPath
+
+
 class Breed(object):
-    def __init__(self, name, dataPath):
-        self.__setName__(name)
-        self.__setDataPath__(dataPath)
-    def __setName__(self, name):
+    def __init__(self, name, datapath_) -> None:
+        self.__set_name__(name)
+        self.__set_datapath__(datapath_)
+
+    def __set_name__(self, name) -> None:
         self.name = name
-    def getName(self):
+
+    def get_name(self) -> str:
         return self.name
-    def __setDataPath__(self, dataPath):
-        self.dataPath = dataPath
-    def getDataPath(self):
-        return self.dataPath
+
+    def __set_datapath__(self, datapath_) -> None:
+        self.datapath = datapath_
+
+    def get_datapath(self) -> dataPath.DataPath:
+        return self.datapath
