459f36fd33a751e9ecb7bbe2c1c66b21a0a84aa0
noalero <noaleron98@gmail.com>
2024-04-09 15:01:24 +0300

    Changed signatures

--- range.py
+++ range.py
@@ -1,18 +1,24 @@
 class Range:
-    def __init__(self, fromIndex, toIndex, edges):
-        self.__setFromIndex__(fromIndex)
-        self.__setToIndex__(toIndex)
-        self.__setEdges__(edges[0], edges[1])
-    def __setFromIndex__(self, index):
-        self.fromIndex = index
-    def getFromIndex(self):
-        return self.fromIndex
-    def __setToIndex__(self, index):
-        self.toIndex = index
-    def getToIndex(self):
-        return self.toIndex
-    def __setEdges__(self, left, right):
-        self.leftEdge = left
-        self.rightEdge = right
-    def getEdges(self):
-        return (self.leftEdge, self.rightEdge)
+    def __init__(self, from_index, to_index, edges) -> None:
+        self.__set_from_index__(from_index)
+        self.__set_to_index__(to_index)
+        self.__set_edges__(edges[0], edges[1])
+
+    def __set_from_index__(self, index) -> None:
+        self.from_index = index
+
+    def get_from_index(self) -> float:
+        return self.from_index
+
+    def __set_to_index__(self, index) -> None:
+        self.to_index = index
+
+    def get_to_index(self) -> float:
+        return self.to_index
+
+    def __set_edges__(self, left, right) -> None:
+        self.left_edge = left
+        self.right_edge = right
+
+    def get_edges(self) -> tuple:
+        return self.left_edge, self.right_edge
