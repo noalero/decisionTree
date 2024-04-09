0232ab852f32556dd3636076184ba7b8e2143abd
noalero <noaleron98@gmail.com>
2024-04-09 14:39:19 +0300

    Changed signatures

--- categoricalFeature.py
+++ categoricalFeature.py
@@ -1,10 +1,15 @@
 import feature
+
+
 class CategoricalFeature(feature.Feature):
-    def __init__(self, name, index, values):
+
+    def __init__(self, name, index, values) -> None:
         feature.Feature.__init__(name, index, values)
-        self.__setBreeds__(values)
-    def __setBreeds__(self, values): # visitor pattern
+        self.__set_breeds__(values)
+
+    def __set_breeds__(self, values) -> None:  # visitor pattern
         breeds = []
         pass
-    def isValueOfBreed(self, str, value): # visitor pattern
-        return str == value
+
+    def is_value_of_breed(self, breed_, value) -> bool:  # visitor pattern
+        return breed_ == value
