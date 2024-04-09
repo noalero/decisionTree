43a2ba047bfdb12adbae2c49f22fcf6adb5bc10b
noalero <noaleron98@gmail.com>
2024-04-09 13:54:43 +0300

    Created new class: DecisionTree

---
+++ decisionTree.py
@@ -0,0 +1,27 @@
+import feature
+import categoricalFeature
+import numericalFeature
+import node
+import breed
+import leaf
+import parent
+import dataPath
+class DecisionTree(object):
+    def __init__(self):
+        pass
+    def __setFeatures__(self):
+        pass
+    def getFeatures(self):
+        return self.features
+    def __setFeature__(self):
+        pass
+    def __setRoot__(self):
+        pass
+    def getRoot(self):
+        return self.root
+    def __createDataBase__(self):
+        pass
+    def __createFeatureTable__(self):
+        pass
+    def retrieveFromDataBase(self, dataPath):
+        pass
