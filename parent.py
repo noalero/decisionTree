db31624f45eea34a5d4cbce4b36c175eef3be7c4
noalero <noaleron98@gmail.com>
2024-04-14 14:19:34 +0300

    Added decision_tree attribute, setter and getter. Added comments and implemented __init__

--- parent.py
+++ parent.py
@@ -1,18 +1,33 @@
 import breed
 import node
+import decisionTree
 
 
 class Parent(breed.Breed):
-    def __init__(self, name, datapath_, feature_):
-        breed.Breed.__init__(self, name, datapath_)
+    def __init__(self, tree, name, datapath_, feature_):
+        self.__set_tree__(tree)
         self.__create_next__(feature_, datapath_)
-        pass
+        breed.Breed.__init__(self, name, datapath_)
 
+# getters & setters of class attributes:
+    # next:
     def __set_next__(self, next_) -> None:
         self.next = next_
 
     def get_next(self) -> node.Node:
         return self.next
 
-    def __create_next__(self, feature, datapath_) -> None:
-        pass
+    def __create_next__(self, feature_, datapath_) -> None:
+        # ToDo
+        # Use [self.tree] to choose the next feature
+        new_feature = 0  # The chosen feature
+        new_datapath = 0 # Add the [new_feature] to the [datapath]
+        next_ = node.Node(self.tree, new_feature, new_datapath)
+        self.__set_next__(next_)
+
+    # tree:
+    def __set_tree__(self, tree) -> None:
+        self.tree = tree
+
+    def get_tree(self) -> decisionTree.DecisionTree:
+        return self.tree
