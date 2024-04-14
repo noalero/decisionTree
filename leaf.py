b64a7a88f910c25ad32bd44fec848c9609b8ee10
noalero <noaleron98@gmail.com>
2024-04-14 14:24:10 +0300

    Added decision_tree attribute, setter and getter. Added comments and implemented __init__

--- leaf.py
+++ leaf.py
@@ -1,13 +1,25 @@
 import breed
+import decisionTree
 
 
 class Leaf(breed.Breed):
-    def __init__(self, name, datapath_):
+    def __init__(self, tree, name, datapath_):
+        self.__set_tree__(tree)
         breed.Breed.__init__(self, name, datapath_)
 
+# getters & setters of class attributes:
+    # answer:
     def __set_answer__(self, answer) -> None:
+        # ToDo
+        # Use [self.tree] and [self.datapath] to get the percentage of each class
         self.answer = answer
 
     def get_answer(self) -> list:
-        # ToDo
         return self.answer
+
+    # tree:
+    def __set_tree__(self, tree) -> None:
+        self.tree = tree
+
+    def get_tree(self) -> decisionTree.DecisionTree:
+        return self.tree
