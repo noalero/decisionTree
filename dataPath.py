3314eab65a2fd8b42d4be6e19c62986f934f637c
noalero <noaleron98@gmail.com>
2024-04-14 14:00:04 +0300

    Added decision_tree attribute, setter and getter. Added comments and implemented __init__

--- dataPath.py
+++ dataPath.py
@@ -4,28 +4,45 @@ import categoricalFeature
 import breed
 import leaf
 import parent
+import decisionTree
 
 
 class DataPath(object):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tree, size, orgnl_path, new_dir) -> None:
+        self.__set_size__(size)
+        self.__set_path__(orgnl_path, new_dir)
+        self.__set_tree__(tree)
 
+# getters & setters of class attributes:
+    # size:
     def __set_size__(self, size) -> None:
         self.size = size
 
     def get_size(self) -> int:
         return self.size
 
-    def __set_path__(self, path) -> None:
-        self.path = path
-        pass
+    # path
+    def __set_path__(self, orgnl_path, new_dir) -> None:
+        # ToDo
+        self.path = orgnl_path.append(new_dir)
+
 
     def get_path(self) -> list:
         return self.path
 
+    # tree:
+    def __set_tree__(self, tree) -> None:
+        self.tree = tree
+
+    def get_tree(self) -> decisionTree.DecisionTree:
+        return self.tree
+
+# addition methods (should delete):
     def add_feature(self, feature_) -> None:
+        # ToDo
         pass
         # resize
 
     def add_breed(self, breed_) -> None:
+        # ToDo
         pass  # resize
