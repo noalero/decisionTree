89a62502c9075c235af216dcc0edaf6b2ad60980
noalero <noaleron98@gmail.com>
2024-05-01 16:12:41 +0300

    changed types

--- dataPath.py
+++ dataPath.py
@@ -1,29 +1,22 @@
 import feature
-import numericalFeature
-import categoricalFeature
-import breed
-import leaf
-import parent
-import decisionTree
+import brange
+from typing import Union
 
 
 class DataPath(object):
-    def __init__(self, size, orgnl_path: list[tuple[feature.Feature, breed.Breed]], new_dir: tuple[feature.Feature, breed.Breed]) -> None:
+    def __init__(self, size, orgnl_path: list[tuple[feature.Feature, Union[brange.Range, str]]], new_dir: tuple[feature.Feature, Union[brange.Range, str]]) -> None:
         self.__set_size__(size)
         self.__set_path__(orgnl_path, new_dir)
 
-# getters & setters of class attributes:
-    # size:
     def __set_size__(self, size) -> None:
         self.size = size
 
-    def get_size(self) -> int:
-        return self.size
-
-    # path
-    def __set_path__(self, orgnl_path: list[tuple[feature.Feature, breed.Breed]], new_dir: tuple[feature.Feature, breed.Breed]) -> None:
+    def __set_path__(self, orgnl_path: list[tuple[feature.Feature, Union[brange.Range, str]]], new_dir: tuple[feature.Feature, Union[brange.Range, str]]) -> None:
         self.path = orgnl_path.append(new_dir)
         self.__set_size__(self.size + 1)
+
+    def get_size(self) -> int:
+        return self.size
 
     def get_path(self) -> list:
         return self.path
