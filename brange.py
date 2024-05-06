536b9c996fd5aef7e2cf387f8d8f57292ac7999a
noalero <noaleron98@gmail.com>
2024-05-06 18:01:56 +0300

    Comment out edges attribute, for now

--- brange.py
+++ brange.py
@@ -1,8 +1,8 @@
 class Range:
-    def __init__(self, from_index, to_index, edges) -> None:
+    def __init__(self, from_index: float, to_index: float) -> None:
         self.__set_from_index__(from_index)
         self.__set_to_index__(to_index)
-        self.__set_edges__(edges[0], edges[1])
+        # self.__set_edges__(edges[0], edges[1])
 
     def __set_from_index__(self, index) -> None:
         self.from_index = index
@@ -16,9 +16,9 @@ class Range:
     def get_to_index(self) -> float:
         return self.to_index
 
-    def __set_edges__(self, left, right) -> None:
-        self.left_edge = left
-        self.right_edge = right
-
-    def get_edges(self) -> tuple:
-        return self.left_edge, self.right_edge
+    # def __set_edges__(self, left, right) -> None:
+    #     self.left_edge = left
+    #     self.right_edge = right
+    #
+    # def get_edges(self) -> tuple:
+    #     return self.left_edge, self.right_edge
