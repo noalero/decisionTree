cf3b55285fc1dff9ea3d799b27bb98bda62f4541
ligma <ligma@sugma.com>
2024-05-18 19:23:49 +0300

    implemented and modified some simple functions. implemented add_child and choose_next_for_children!! need to be tested! need to implement __choose_feature_for_child__

--- node.py
+++ node.py
@@ -1,33 +1,44 @@
-import numpy as np
-
+import brange
 import dataPath
 import feature
-import breed
-import decisionTree
+import node
 
 
 class Node(object):
-    def __init__(self, feature_, datapath_) -> None:
+    def __init__(self, feature_: feature.Feature, datapath_: dataPath.DataPath) -> None:
         self.__set_feature__(feature_)
         self.__set_datapath__(datapath_)
+        self.__init_num_of_children__()  # for initialization
+        self.__set_children__()
 
-    def __set_datapath__(self, datapath_) -> None:
+    def __set_datapath__(self, datapath_: dataPath.DataPath) -> None:
         self.datapath = datapath_
 
-    def __set_feature__(self, feature_) -> None:
+    def __set_feature__(self, feature_: feature.Feature) -> None:
         self.feature = feature_
 
-    def __set_num_of_children__(self, num_of_children) -> None:
-        self.num_of_children = num_of_children
+    def __init_num_of_children__(self) -> None:
+        self.num_of_children = 0
+
+    def __increase_num_of_children__(self) -> None:
+        self.num_of_children += 1
+
+    def __choose_feature_for_child__(self, breed_: str | brange.Range, feat_list: list[feature.Feature])\
+            -> tuple[feature.Feature, float]:
+        # TODO
+        pass
 
     def __set_children__(self) -> None:
-        # ToDo
-        pass
+        self.children: list[node.Node] = []
 
-    def __set_child__(self, child) -> None:
-        # ToDo
-        # __create_child__ ?
-        pass
+    def __add_child__(self, breed_: str | brange.Range, child_feature: feature.Feature) -> None:
+        # ToDo: test
+        # child_feature = self.__choose_feature_for_child__(breed_, feat_list)
+        new_dir = (self.feature, breed_)
+        child_data_path = dataPath.DataPath(self.datapath.get_size(), self.datapath.get_path(), new_dir)
+        new_child = node.Node(child_feature, child_data_path)
+        self.children.append(new_child)
+        self.__increase_num_of_children__()
 
     def get_feature(self) -> feature.Feature:
         return self.feature
@@ -38,20 +49,47 @@ class Node(object):
     def get_num_of_children(self) -> int:
         return self.num_of_children
 
-    def get_children(self) -> breed.Breed:
-        # ToDo
-        pass
-        # return self.children
+    def get_children(self) -> list[node.Node]:
+        return self.children
 
-    def get_child(self, breed_name) -> breed.Breed:
-        # ToDo
-        pass
+    def get_child(self, breed_: str | brange.Range) -> node.Node:
+        for child in self.children:
+            dp = child.get_datapath()
+            dp_size = dp.get_size()
+            last_dir = dp.get_path()[dp_size - 1]
+            if last_dir[1] == breed_:
+                return child
+        raise ValueError(f"Child of breed {breed_} not found")
 
-    def add_child(self, breed_, datapath_, arg) -> None:
-        # ToDo
-        pass
-        # create parent / leaf, visitor pattern
-
-    def choose_next_for_children(self) -> None:
-        # ToDo
-        pass
+    def choose_next_for_children(self, feat_list: list[feature.Feature]) -> None:
+        # ToDo: test
+        feat_dict: dict[feature.Feature, tuple[str | brange.Range, float]] = {}  # key: feature, value: breed, IG
+        unchosen_breeds: list[str | brange.Range] = self.feature.get_breeds()
+        curr_feature_list: list[feature.Feature] = feat_list
+        i: int = 0
+        org_size = len(unchosen_breeds)
+        for brd in unchosen_breeds:
+            if i == org_size:
+                for feat, tup in feat_dict:
+                    # add children from [feat_dict]:
+                    self.__add_child__(tup[0], feat)
+                    # empty [feat_dict]:
+                    del feat_dict[feat]
+                    # remove features from [curr_feature_list]:
+                    curr_feature_list.remove(feat)
+                # new value to org_size:
+                org_size = len(unchosen_breeds)
+                # restart i:
+                i = 0
+            i += 1
+            temp_feat, temp_ig = self.__choose_feature_for_child__(brd, curr_feature_list)
+            unchosen_breeds.remove(brd)
+            if temp_feat in feat_dict:
+                old_breed, old_ig = feat_dict[temp_feat]
+                if temp_ig > old_ig:
+                    feat_dict[temp_feat] = (brd, temp_ig)
+                    unchosen_breeds.append(old_breed)
+                else:
+                    unchosen_breeds.append(brd)
+            else:
+                feat_dict[temp_feat] = (brd, temp_ig)
