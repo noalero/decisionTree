b578c4342939d99e3546e6ef3442d24ea671ef5c
ligma <ligma@sugma.com>
2024-05-18 14:13:55 +0300

    type of path

--- dataPath.py
+++ dataPath.py
@@ -4,15 +4,16 @@ from typing import Union
 
 
 class DataPath(object):
-    def __init__(self, size, orgnl_path: list[tuple[feature.Feature, Union[brange.Range, str]]], new_dir: tuple[feature.Feature, Union[brange.Range, str]]) -> None:
+    def __init__(self, size, orgnl_path: list[tuple[feature.Feature, brange.Range | str]],
+                 new_dir: tuple[feature.Feature, brange.Range | str]) -> None:
         self.__set_size__(size)
         self.__set_path__(orgnl_path, new_dir)
 
     def __set_size__(self, size) -> None:
         self.size = size
 
-    def __set_path__(self, orgnl_path: list[tuple[feature.Feature, Union[brange.Range, str]]],
-                     new_dir: tuple[feature.Feature, Union[brange.Range, str]]) -> None:
+    def __set_path__(self, orgnl_path: list[tuple[feature.Feature, brange.Range | str]],
+                     new_dir: tuple[feature.Feature, brange.Range | str]) -> None:
         self.path = orgnl_path.append(new_dir)
         self.__set_size__(self.size + 1)
 
